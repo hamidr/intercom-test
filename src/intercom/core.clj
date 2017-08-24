db44bbf3e472b334cb93e7743a0d4f9057199f1c
hamidr <hamidr.dev@gmail.com>
2017-08-24 10:08:53 +0430

    Parse a given file by arguments containing a list of employees, find those within 100km of dublin, Sort them by thier IDs and print them:
    ID: 4 Name: Ian Kehoe
    ID: 5 Name: Nora Dempsey
    ID: 6 Name: Theresa Enright
    ID: 8 Name: Eoin Ahearn
    ID: 11 Name: Richard Finnegan
    ID: 12 Name: Christina McArdle
    ID: 13 Name: Olive Ahearn
    ID: 15 Name: Michael Ahearn
    ID: 17 Name: Patricia Cahill
    ID: 23 Name: Eoin Gallagher
    ID: 24 Name: Rose Enright
    ID: 26 Name: Stephen McArdle
    ID: 29 Name: Oliver Ahearn
    ID: 30 Name: Nick Enright
    ID: 31 Name: Alan Behan
    ID: 39 Name: Lisa Ahearn

--- src/intercom/core.clj
+++ src/intercom/core.clj
@@ -1,1 +1,62 @@
-(ns intercom.core)
+(ns intercom.core
+  (:gen-class)
+  (:require [clojure.string :as string]
+            [clojure.data.json :as json]
+            [intercom.geo :as geo]))
+
+(defn- value-reader
+  [key value]
+  (if (or (= key :latitude) (= key :longitude))
+    (read-string value)
+    value))
+
+(defn- convert-line
+  [line]
+  (json/read-str line
+      :key-fn keyword
+      :value-fn value-reader))
+
+(defn- convert-lines
+  [lines]
+  (map convert-line lines))
+
+(def ^:private dublin
+  {:latitude 53.339428 :longitude -6.257664})
+
+(defn- tprint
+  [customers]
+  (doseq [c customers]
+    (let [{id :user_id name :name} c
+          item (str "ID: " id " Name: " name)]
+      (println item))))
+
+(defn- valid?
+  [{id   :user_id
+    name :name
+    lat  :latitude
+    long :longitude}]
+  (not
+   (or (nil? name)
+       (nil? id)
+       (nil? lat)
+       (nil? long))))
+
+(defn- arg-or-default
+  [args default]
+  (let [addr (first args)]
+    (if (nil? addr)
+      default
+      addr)))
+
+(defn -main
+  [& args]
+  (->>
+   (arg-or-default args "data/customer.json")
+   slurp                         ;; read file
+   string/split-lines            ;; split by line into separate elements
+   convert-lines                 ;; convert to json
+   (filter valid?)               ;; filter only valid ones
+   (geo/select-within-radius 100 dublin) ;; select those in 100km radius
+   (sort-by :user_id)            ;; sort by their :user_id
+   tprint                        ;; print the result
+   ))
